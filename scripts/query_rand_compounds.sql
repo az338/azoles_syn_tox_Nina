aa02acf2cdf5b07af3dfeae21c4af4ed53ce7be2
Azedine Zoufir <az338@rover.ch.cam.ac.uk>
2015-11-25 11:57:23 +0000

    query to extract random compounds, split into small sets, started analysis of smartcyp scores

---
+++ scripts/query_rand_compounds.sql
@@ -0,0 +1,1 @@
+ mysql -u az338 -p -e 'select stdsmiles from compounds order by rand() limit 10000;' pidgin > ucc-fileserver/ucc_az/azoles_synergistic_tox_Nina/data/random_chem.smi
---
+++ scripts/random_chem_split.sh
@@ -0,0 +1,4 @@
+head -n 250 random_chem_10000.smi > random_chem_250.smi
+head -n 2000 random_chem_10000.smi | tail -n 250  > random_chem_250_2.smi
+head -n 750 random_chem_10000.smi | tail -n 250  > random_chem_250_3.smi
+head -n 1000 random_chem_10000.smi | tail -n 250  > random_chem_250_4.smi
